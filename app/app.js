7e7531c6e89b41123e1eb6a700b7c1857a33ad6d
Kyoduke <45364101+Kyoduke@users.noreply.github.com>
2018-12-17 18:49:01 -0200

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 var btn = document.getElementsByClassName("buttons");
 var displayColor = document.getElementsByTagName("h2")[0]
-
 var colors = generateRandomColors();
-
 var color = pickColor().toLowerCase();
+var mudarCores = document.getElementById("mudar");
+var msg = document.getElementById("message")
 
-displayColor.textContent = color
+displayColor.textContent = color.toUpperCase()
 
 for(n in colors) {
     btn[n].style.backgroundColor = colors[n]
@@ -16,6 +16,15 @@ console.log(generateRandomColor());
 for(n=0; n<btn.length; n++) {
     btn[n].addEventListener("click", comparar)
 }
+
+mudarCores.addEventListener("click", reset)
+
+
+
+
+
+
+
 
 
 
@@ -41,8 +50,28 @@ function pickColor() {
 
 function comparar() {
     if(this.style.backgroundColor == color) {
-        console.log("CORRECT")
+        for(i=0; i<btn.length;i++) {
+            btn[i].style.backgroundColor = color;
+        }
+        mudarCores.textContent = "Novas Cores";
+        msg.textContent = "Acertou!"
     } else {
+        this.style.backgroundColor = "#232323";
         console.log("INCORRECT")
+        msg.textContent = "Tente novamente"
+    }
+}
+
+function reset() {
+    colors = generateRandomColors();
+
+    color = pickColor().toLowerCase();
+
+    displayColor.textContent = color.toUpperCase();
+
+    mudarCores.textContent = "Novas cores"
+
+    for(n in colors) {
+        btn[n].style.backgroundColor = colors[n]
     }
 }
